ebfbc54165c671b66942cb5663461a6d87d6a386
Vitalii Zelenyi <vzntst@gmail.com>
2020-07-28 21:03:54 +0300

    second commit

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,5 +1,4 @@
-import updateClockFace from './updateClockFace';
-
+import pad from './pad';
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
@@ -7,8 +6,30 @@ class CountdownTimer {
   }
 
   startTimer() {
-    const counterBlock = document.querySelector(this.selector);
     this.targetTime = new Date(this.targetDate);
+    const counterBlock = document.querySelector(this.selector);
+    function updateClockFace(time) {
+      // const date = new Date(time);
+
+      const refs = {
+        daysCounter: counterBlock.querySelector('span[data-value="days"'),
+        hoursCounter: counterBlock.querySelector('span[data-value="hours"'),
+        minsCounter: counterBlock.querySelector('span[data-value="mins"'),
+        secsCounter: counterBlock.querySelector('span[data-value="secs"'),
+      };
+
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      refs.daysCounter.textContent = days;
+      refs.hoursCounter.textContent = hours;
+      refs.minsCounter.textContent = mins;
+      refs.secsCounter.textContent = secs;
+    }
     setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = this.targetTime - currentTime;
@@ -17,9 +38,15 @@ class CountdownTimer {
   }
 }
 
-const timer = new CountdownTimer({
+const timer1 = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Aug 24, 2020'),
+  targetDate: new Date('Aug 1, 2020'),
 });
 
-timer.startTimer();
+const timer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Aug 21, 2020'),
+});
+
+timer1.startTimer();
+timer2.startTimer();
